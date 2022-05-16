1e90e932207c044bd6aeddfe6357a5a6343b18ac
fantaric <riccafantacci@gmail.com>
2022-05-16 18:49:26 +0200

    primo commit

---
+++ Synnvhro/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+       Synchro s1 = new Synchro();
+       Synchro s2 = new Synchro();
+
+       
+    }
+}
---
+++ Synnvhro/src/Synchro.java
@@ -0,0 +1,35 @@
+public class Synchro implements Runnable{
+    
+
+    public void eliminazione()
+    {
+
+    }
+
+    public void salvataggio()
+    {
+
+    }
+
+    public void formattazione()
+    {
+        
+    }
+
+    public void signal()
+    {
+
+    }
+
+    public void wait(0L,L)
+    {
+
+    }
+
+    
+
+    public void run()
+    {
+
+    }
+}
